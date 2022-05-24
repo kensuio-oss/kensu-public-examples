768d3e4404f8d3cc704f1b1afc4b613b1985a196
Andy Petrella <andy.petrella@kensu.io>
2022-05-24 15:04:18 +0200

    add clean docker, rm dbt target, simplified python

---
+++ Marketing_Campaign/pandas-load-data/load_campain.py
@@ -0,0 +1,14 @@
+import urllib3, datetime, sys
+urllib3.disable_warnings()
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+campaign = sys.argv[1]
+
+customers = pd.read_csv(campaign+"_campaign/customer_list.csv")
+engine = create_engine('postgresql://postgres:postgresPass@host.docker.internal:5432/testme')
+customers.to_sql('customers', engine,index=False,if_exists='replace')
+orders = pd.read_csv(campaign+"_campaign/orders.csv")
+orders = orders.rename(columns={'id':"customer_id"})
+orders.to_sql('orders', engine,index=False,if_exists='replace')
