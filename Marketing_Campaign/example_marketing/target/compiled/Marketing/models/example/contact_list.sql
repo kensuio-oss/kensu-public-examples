b1aa8b29719d7b82b8a55fb06cfbe380d018abf0
Andy Petrella <andy.petrella@kensu.io>
2022-05-24 15:29:22 +0200

    align

--- Marketing_Campaign/example_marketing/target/compiled/Marketing/models/example/contact_list.sql
+++
@@ -1,5 +0,0 @@
-
-
-
-select first_name, phone
-from "testme"."testme_schema"."orders_and_customers"
--- Marketing_Campaign/example_marketing/target/compiled/Marketing/models/example/email_list.sql
+++
@@ -1,7 +0,0 @@
-
-
--- FIXME: support views
--- Use the `ref` function to select from other models
-
-select email
-from "testme"."testme_schema"."orders_and_customers"
