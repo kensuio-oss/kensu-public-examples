ac6ea3ce3bb3f56049e87c91baa3a807825ac336
Andy Petrella <andy.petrella@kensu.io>
2022-05-24 15:23:02 +0200

    simplify py

---
+++ Marketing_Campaign/clean-docker.sh
@@ -0,0 +1,2 @@
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
--- Marketing_Campaign/pandas-load-data/load_campain.py
+++ Marketing_Campaign/pandas-load-data/load_campain.py
@@ -1,4 +1,4 @@
-import urllib3, datetime
+import urllib3, datetime, sys
 urllib3.disable_warnings()
 
 from kensu.utils.kensu_provider import KensuProvider
@@ -8,9 +8,11 @@ k = KensuProvider().initKensu(project_names=["Marketing"],process_name='Python :
 import kensu.pandas as pd
 from sqlalchemy import create_engine
 
-customers = pd.read_csv("first_campaign/customer_list.csv")
+campaign = sys.argv[1]
+
+customers = pd.read_csv(campaign+"_campaign/customer_list.csv")
 engine = create_engine('postgresql://postgres:postgresPass@host.docker.internal:5432/testme')
 customers.to_sql('customers', engine,index=False,if_exists='replace')
-orders = pd.read_csv("first_campaign/orders.csv")
+orders = pd.read_csv(campaign+"_campaign/orders.csv")
 orders = orders.rename(columns={'id':"customer_id"})
 orders.to_sql('orders', engine,index=False,if_exists='replace')
