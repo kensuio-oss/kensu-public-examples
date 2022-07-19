e6f617b4e2f65441e0c242dcaf6e51e7c9547d1f
Sammy <sammy.elkhammal@kensu.io>
2022-07-19 10:15:24 +0200

    update Kensu lib 1.9.2.1

--- Financial_Data_Report/python_code/data_ingestion.py
+++ Financial_Data_Report/python_code/data_ingestion.py
@@ -19,12 +19,12 @@ import kensu.pandas as pd
 
 #Core Script: Concatenate data sources in order to create a central repository: monthly_assets
 
-Apple = pd.read_csv("../datasources/%s/%s/Apple.csv"%(year,month),parse_dates=['Date'])
-Buzzfeed = pd.read_csv("../datasources/%s/%s/Buzzfeed.csv"%(year,month),parse_dates=['Date'])
-EURUSD = pd.read_csv("../datasources/%s/%s/EURUSD.csv"%(year,month),parse_dates=['Date'])
-Microsoft = pd.read_csv("../datasources/%s/%s/Microsoft.csv"%(year,month),parse_dates=['Date'])
-iMetal = pd.read_csv("../datasources/%s/%s/iMetal.csv"%(year,month),parse_dates=['Date'])
-AppTech = pd.read_csv("../datasources/%s/%s/AppTech.csv"%(year,month),parse_dates=['Date'])
+Apple = pd.read_csv("../datasources/%s/%s/Apple.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
+Buzzfeed = pd.read_csv("../datasources/%s/%s/Buzzfeed.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
+EURUSD = pd.read_csv("../datasources/%s/%s/EURUSD.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
+Microsoft = pd.read_csv("../datasources/%s/%s/Microsoft.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
+iMetal = pd.read_csv("../datasources/%s/%s/iMetal.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
+AppTech = pd.read_csv("../datasources/%s/%s/AppTech.csv"%(year,month),parse_dates=['Date'],dtype={'Symbol':'category'})
 
-monthly_assets = pd.concat([Apple,Buzzfeed,EURUSD,Microsoft,iMetal,AppTech])
+monthly_assets = pd.concat([Apple,Buzzfeed,EURUSD,Microsoft,iMetal,AppTech]).astype({"Symbol": 'category'})
 monthly_assets.to_csv("../datasources/%s/%s/monthly_assets.csv"%(year,month),index=False)
