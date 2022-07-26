d4bc63844553df40bdb7d4a468d6b26f63d516c8
Andy Petrella <andy.petrella@kensu.io>
2022-07-26 15:30:27 +0200

    mv run

---
+++ Marketing_Campaign/run.sh
@@ -0,0 +1,13 @@
+echo "** clean up **"
+./clean-docker.sh
+
+
+echo "** install **"
+./installation.sh
+
+
+echo "** FIRST EXEC **"
+./first_execution.sh
+
+echo "** SECOND EXEC **"
+./second_execution.sh
