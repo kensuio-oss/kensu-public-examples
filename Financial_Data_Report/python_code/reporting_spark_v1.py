35fb2d186c4bd5011cc3d78e725e39859efd4593
Leszek Gruchała <leszek.gruchala@kensu.io>
2022-10-11 14:47:44 +0200

    Prepare the code for the new major releases of KensuPy and Spark-Collector

--- Financial_Data_Report/python_code/reporting_spark_v1.py
+++ Financial_Data_Report/python_code/reporting_spark_v1.py
@@ -23,7 +23,7 @@ from kensu.pyspark import init_kensu_spark
 
 #Inject Kensu agent in spark session: Add the path to the .jar to the SparkSession
 spark = SparkSession.builder.appName("Example")\
-    .config("spark.driver.extraClassPath", "../lib/kensu-spark-collector-1.0.0-SNAPSHOT_spark-3.0.1.jar")\
+    .config("spark.driver.extraClassPath", "../lib/kensu-spark-collector-1.0.0-rc466_spark-3.0.1.jar")\
     .getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
 
